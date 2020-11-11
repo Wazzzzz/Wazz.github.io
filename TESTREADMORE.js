0cdc2ababb0572ee1a0f9b6093f8be56c4550a0f
Fawaz Abdurrahman <58974605+Wazzzzz@users.noreply.github.com>
2020-11-11 19:48:27 +0700

    untuk readmore
    nambah js

--- TESTREADMORE.js
+++ TESTREADMORE.js
@@ -14,4 +14,20 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("myModal2");
+
+var btn2 = document.getElementById("myBtn2");
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
 }
